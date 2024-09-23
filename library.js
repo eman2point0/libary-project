744eeef2df4ae6219288d59fd13213a132120209
eman2point0 <eman2point0@yahoo.com>
2024-09-23 18:29:27 -0400

    Read button is now associated with whether user has read book or not. Green for read, red for not read.

--- library.js
+++ library.js
@@ -5,6 +5,7 @@ const form = document.querySelector(".add-book-form");
 const addBtn = document.querySelector("#addBtn");
 const dialog = document.querySelector("dialog");
 const submitFormBtn = document.getElementById("submitBtn");
+const bookSection = document.querySelector(".book-section");
 
 /*Inputs for form*/
 function Book(title, author, pages, read) {
@@ -25,10 +26,16 @@ addBtn.onclick = () => {
   dialog.showModal();
 };
 
+const readBtns = document.querySelectorAll(".readBtn");
+readBtns.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    console.log(event.target);
+  });
+});
+
 const removeBtns = document.querySelectorAll(".removeBtn");
 removeBtns.forEach((button) => {
   button.addEventListener("click", (event) => {
-    const bookSection = document.querySelector(".book-section");
     const book = button.parentElement.parentElement;
     console.log("Remove Button has been clicked!");
     bookSection.removeChild(book);
@@ -36,16 +43,14 @@ removeBtns.forEach((button) => {
 });
 
 submitFormBtn.onclick = () => {
-  const bookSection = document.querySelector(".book-section");
   const titleInput = document.querySelector("#bookTitle").value;
   const authorInput = document.querySelector("#inputAuthor").value;
   const pageCount = document.querySelector("#pageNum").value;
-  const read = document.querySelector(
-    'input[name="read_attribute"]:checked'
-  ).value;
+  const read = JSON.parse(
+    document.querySelector('input[name="read_attribute"]:checked').value
+  );
 
   const newBook = new Book(titleInput, authorInput, pageCount, read);
-
   updateLibrary(newBook);
   addBookToPage(newBook);
   form.reset();
@@ -53,7 +58,7 @@ submitFormBtn.onclick = () => {
 };
 
 function validateReadOption(book) {
-  if (book.read === "true") {
+  if (book.read) {
     return "green";
   } else {
     return "red";
@@ -91,18 +96,29 @@ function addBookToPage(book) {
   const buttonSection = document.createElement("div");
   buttonSection.classList.add("book-options");
 
+  /*Read Button */
   const readBtn = document.createElement("input");
   readBtn.setAttribute("type", "button");
-  readBtn.setAttribute("value", "Read");
-  readBtn.classList.add("readBtn");
 
+  readBtn.classList.add("readBtn");
+  readBtn.setAttribute("value", book.read ? "Mark Unread" : "Mark Read");
+  console.log(book);
+  console.log(book.read + " " + validateReadOption(book));
+  readBtn.style.backgroundColor = validateReadOption(book);
+  readBtn.addEventListener("click", () => {
+    console.log("Read Button has been clicked!");
+    book.read = !book.read;
+    console.log("Book read is now: " + book.read);
+    readBtn.style.backgroundColor = book.read ? "green" : "red";
+    readBtn.value = book.read ? "Mark Unread" : "Mark Read";
+  });
+  /*Remove Button */
   const removeBtn = document.createElement("input");
   removeBtn.setAttribute("type", "button");
   removeBtn.setAttribute("value", "Remove");
   removeBtn.classList.add("removeBtn");
   removeBtn.addEventListener("click", (event) => {
     console.log("Remove Button has been clicked!");
-    const bookSection = document.querySelector(".book-section");
     const book = removeBtn.parentElement.parentElement;
     bookSection.removeChild(book);
   });
@@ -118,14 +134,12 @@ const curiousGeorgeBook = new Book(
   5,
   true
 );
-
 const fahrenheitBook = new Book("Fahrenheit 451", "Ray Bradbury", 242, true);
 const eragonBook = new Book("Eragon", "Chirstopher Paolini", 509, true);
 myLibrary.push(curiousGeorgeBook);
 myLibrary.push(eragonBook);
-addBookToPage(fahrenheitBook);
 myLibrary.push(fahrenheitBook);
 
 myLibrary.forEach((book) => {
-  book.bookInfo();
+  addBookToPage(book);
 });
